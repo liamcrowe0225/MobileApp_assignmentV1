1e56e063be6613ea970836506cd912988f80d6f1
liamc <20093470@mail.wit.ie>
2023-02-24 13:31:42 +0000

    Added generic import files in ListFixtures.kt

--- app/src/main/java/com/example/mobileapp_assignmentv1/activities/ListFixtures.kt
+++ app/src/main/java/com/example/mobileapp_assignmentv1/activities/ListFixtures.kt
@@ -1,4 +1,14 @@
 package com.example.mobileapp_assignmentv1.activities
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+
 class ListFixtures {
+
 }

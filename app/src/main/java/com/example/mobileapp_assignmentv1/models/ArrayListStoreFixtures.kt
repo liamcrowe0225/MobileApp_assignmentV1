a5518c3d8de77fd19218470ae7c78af5fbc3a68f
liamc <20093470@mail.wit.ie>
2023-02-24 12:16:51 +0000

    CRUD system for Arraylist is created and also function call in FixturesArrayFunCall is created

--- app/src/main/java/com/example/mobileapp_assignmentv1/models/ArrayListStoreFixtures.kt
+++ app/src/main/java/com/example/mobileapp_assignmentv1/models/ArrayListStoreFixtures.kt
@@ -1,2 +1,45 @@
 package com.example.mobileapp_assignmentv1.models
 
+import timber.log.Timber.i
+
+var Id = 0L
+internal fun getId(): Long {
+    return Id++
+}
+class ArrayListStoreFixtures : FixturesArrayFunCall {
+
+    val fixture = ArrayList<Fixtures>()
+
+    override fun findAll(): List<Fixtures> {
+        return fixture
+    }
+
+    override fun create(fixtures: Fixtures) {
+        fixtures.idClub = getId()
+        fixture.add(fixtures)
+        logAll()
+    }
+
+    override fun delete(club: Fixtures) {
+        //use the id to find it call actualClub
+        var actualClub: Fixtures? = fixture.find { p -> p.idClub == club.idClub }
+        fixture.remove(actualClub)
+    }
+
+    override fun update(placemark: Fixtures) {
+        var foundCriteria: Fixtures? = fixture.find { p -> p.idClub == placemark.idClub }
+        if (foundCriteria != null) {
+            foundCriteria.team1 = placemark.team1
+            foundCriteria.team2 = placemark.team2
+            foundCriteria.score1 = placemark.score1
+            foundCriteria.score2 = placemark.score2
+            foundCriteria.venue = placemark.venue
+            foundCriteria.date = placemark.date
+            logAll()
+        }
+    }
+
+    private fun logAll() {
+        fixture.forEach { i("$it") }
+    }
+}
--- app/src/main/java/com/example/mobileapp_assignmentv1/models/FixturesArrayFunCall.kt
+++ app/src/main/java/com/example/mobileapp_assignmentv1/models/FixturesArrayFunCall.kt
@@ -1,4 +1,8 @@
 package com.example.mobileapp_assignmentv1.models
 
 interface FixturesArrayFunCall {
+    fun findAll(): List<Fixtures>
+    fun create(placemark: Fixtures)
+    fun update(placemark: Fixtures)
+    fun delete(club: Fixtures)
 }

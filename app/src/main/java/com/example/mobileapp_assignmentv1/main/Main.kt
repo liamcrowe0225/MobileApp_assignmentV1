dd9d1e82f73574eb645fe5f2b0f55c9f6ef43116
liamc <20093470@mail.wit.ie>
2023-02-24 12:29:52 +0000

    Added the code for the Main.kt file but it is not working and an issue has been raised.

--- app/src/main/java/com/example/mobileapp_assignmentv1/main/Main.kt
+++ app/src/main/java/com/example/mobileapp_assignmentv1/main/Main.kt
@@ -1,4 +1,15 @@
 package com.example.mobileapp_assignmentv1.main
 
+import com.example.mobileapp_assignmentv1.models.ArrayListStoreFixtures
+import timber.log.Timber
+
 class Main {
+    val fixtures = ArrayListStoreFixtures()
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+//        placemarks.add(PlacemarkModel("One", "About one..."))
+//        placemarks.add(PlacemarkModel("Two", "About two..."))
+//        placemarks.add(PlacemarkModel("Three", "About three..."))
+    }
 }

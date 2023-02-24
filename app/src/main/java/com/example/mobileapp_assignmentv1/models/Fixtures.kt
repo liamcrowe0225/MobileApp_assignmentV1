e36f4f5fcacd6241e46ac473fcefa1177e9e9257
liamc <20093470@mail.wit.ie>
2023-02-24 12:08:28 +0000

    Added a data class to the Fixtures Kotlin file

--- app/src/main/java/com/example/mobileapp_assignmentv1/models/Fixtures.kt
+++ app/src/main/java/com/example/mobileapp_assignmentv1/models/Fixtures.kt
@@ -1,4 +1,14 @@
 package com.example.mobileapp_assignmentv1.models
 
-class Fixtures {
-}
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Fixtures(var idClub: Long = 0,
+                     var team1: String = "",
+                     var team2: String = "",
+                     var score1: Int = 0,
+                     var score2: Int = 0,
+                     var venue:  String = "",
+                     var date: String = ""
+) : Parcelable

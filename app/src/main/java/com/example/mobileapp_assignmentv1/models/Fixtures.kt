0f8ae3f89c058986c80d07e271428b1f1e13eda7
liamc <20093470@mail.wit.ie>
2023-03-22 15:10:35 +0000

    Updated another XML file along with adding one image to the data class.

--- app/src/main/java/com/example/mobileapp_assignmentv1/models/Fixtures.kt
+++ app/src/main/java/com/example/mobileapp_assignmentv1/models/Fixtures.kt
@@ -1,5 +1,6 @@
 package com.example.mobileapp_assignmentv1.models
 
+import android.net.Uri
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import java.text.SimpleDateFormat
@@ -12,7 +13,9 @@ data class Fixtures(var idClub: Long = 0,
                      var score1: Int = 0,
                      var score2: Int = 0,
                      var venue:  String = "",
-                     var date: String = ""
+                     var date: String = "",
+                     var image: Uri = Uri.EMPTY
+                     //var image2: Uri = Uri.EMPTY
 ) : Parcelable /*{
     fun getFormattedDate(): String {
         val dateObj = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(date)
